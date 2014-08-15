be80a106179830c7a60bdc45e9406a5838596da9
ricec123 <clintonrice@clintons-imac.attlocal.net>
2014-08-14 23:08:18 -0500

    Update to variable names, usability comments
    Modified descriptive comments to make intended use of the functions
    more self-explanatory.
    Modified references to matrix inverse to make the names more
    descriptive.

--- cachematrix.R
+++ cachematrix.R
@@ -2,41 +2,44 @@
 ## and allow for caching and management of it and its inverse.
 ## Recommended use:
 ##     initialize with cm <- makeCacheMatrix(x)
-##     use cm$get() to examine matrix contents
+##     use cm$get() to retrieve matrix contents
 ##     use cm$set(y) to reset matrix value and inverse
-##     use cacheSolve(cm) function to get and/or set inverse;
-##     do not use cm$getinverse() or cm$setinverse(inv) directly
+##     use cacheSolve(cm) function to get and/or set inverse of cm.
+##
+##     DO NOT use cm$getinverse() or cm$setinverse(inv) directly, as
+##         they are intended for use only by cacheSolve
 
-## Given a matrix assumed to be invertible, produces a placeholder (m)
+## Given a matrix assumed to be invertible, produces a placeholder (cmi)
 ## for its inverse and returns a list of management functions
+## Among those functions, only get and set should be used directly.
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    cmi <- NULL
     set <- function(y) {
         x <<- y
-        m <<- NULL
+        cmi <<- NULL
     }
     get <- function() x
-    setinverse <- function(inv) m <<- inv
-    getinverse <- function() m
+    setinverse <- function(nv) cmi <<- nv
+    getinverse <- function() cmi
     list(set = set, get = get,
     setinverse = setinverse,
     getinverse = getinverse)
 }
 
 
-## Given x as a list product of makeCacheMatrix, checks for an existing
-## inverse to return and solves for it if none exists yet.
+## Given x as a list product of makeCacheMatrix, checks for an calculated
+## inverse and solves for it if none is stored yet, returning the inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
+        nv <- x$getinverse()
+        if(!is.null(nv)) {
             message("getting cached data")
-            return(m)
+            return(nv)
         }
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        nv <- solve(data, ...)
+        x$setinverse(nv)
+        nv
 }
